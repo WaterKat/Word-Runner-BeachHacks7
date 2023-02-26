84c516f616ec3303ed31fd69883268bf1b385cbf
WaterKatLaptop\WaterKat <WaterKatLLC@gmail.com>
2023-02-26 14:00:02 -0800

    Hello world fix

--- Assets/helloworldscript.cs
+++ Assets/helloworldscript.cs
@@ -7,50 +7,55 @@ using System.IO;
 
 public class helloworldscript : MonoBehaviour
 {
-    public UnityEvent jump;
-    public UnityEvent kick;
-    public UnityEvent duck;
-    
+    public UnityEvent jumpEvent;
+    public UnityEvent kickEvent;
+    public UnityEvent duckEvent;
+
     string jump;
     string kick;
     string duck;
+    string input;
     // Start is called before the first frame update
+    List<string> listofWords = new List<string>();
+
     void Start()
     {
-        
+
         var lines = System.IO.File.ReadLines(@"Assets/message.txt");
         //foreach (var item in lines) {Debug.Log(item);}
-        List <string> listofWords = new List<string>();
-        foreach(var item in lines)
+        foreach (var item in lines)
         {
             listofWords.Add(item);
         }
         jump = (string)listofWords[Random.Range(0, listofWords.Count)];
         kick = (string)listofWords[Random.Range(0, listofWords.Count)];
         duck = (string)listofWords[Random.Range(0, listofWords.Count)];
-        Debug.Log("kick: "+kick);
-        Debug.Log("jump: "+jump);
-        Debug.Log("duck: "+duck);
+        Debug.Log("kick: " + kick);
+        Debug.Log("jump: " + jump);
+        Debug.Log("duck: " + duck);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(input == jump)
+        if (input == jump)
         {
-            jump.Invoke();
+            jumpEvent.Invoke();
         }
-        if else(input == kick)
+        else if (input == kick)
         {
-            kick.Invoke();
+            kickEvent.Invoke();
         }
-        else  {duck.Invoke()}
+        else
+        {
+            duckEvent.Invoke();
+        }
     }
 
     public void WordReroll()
     {
-    jump = (string)listofWords[Random.Range(0, listofWords.Count)];
-    kick = (string)listofWords[Random.Range(0, listofWords.Count)];
-    duck = (string)listofWords[Random.Range(0, listofWords.Count)];
+        jump = (string)listofWords[Random.Range(0, listofWords.Count)];
+        kick = (string)listofWords[Random.Range(0, listofWords.Count)];
+        duck = (string)listofWords[Random.Range(0, listofWords.Count)];
     }
 }
