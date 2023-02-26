debdfdf8bc425d628b0b4403552d11743d56b4d9
huy2000vu <104468409+huy2000vu@users.noreply.github.com>
2023-02-26 11:08:26 -0800

    New changes

---
+++ Assets/helloworldscript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+//using random
+
+public class helloworldscript : MonoBehaviour
+{
+    string jump;
+    string kick;
+    string duck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] lines = System.IO.File.ReadAllLines(@"Assets/_Project/message.txt");
+        int randomNum = Random.Range(0,50);
+        foreach (var item in lines) 
+        {
+            Debug.Log(item);
+            //Debug.Log(lines[randomNum]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
