2c1d1908b1490980eccbd10c6aafcab5986be557
WaterKatLaptop\WaterKat <WaterKatLLC@gmail.com>
2023-02-25 22:14:14 -0800

    Added camera follow

--- Assets/_Project/Player/FollowPlayer.cs
+++ Assets/_Project/Player/FollowPlayer.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] Transform target;
-    public float offset = -2;
+    public float offset_x = -2;
+    public float offset_y = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +16,13 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        while (Mathf.Abs(target.position.x - (transform.position.x+offset) )  > 2)
+        while (Mathf.Abs(target.position.x - (transform.position.x+offset_x) )  > 2)
         {
-            transform.position += new Vector3(Mathf.Sign(target.position.x - (transform.position.x+offset) ) * Time.deltaTime, 0, 0 );
+            transform.position += new Vector3(Mathf.Sign(target.position.x - (transform.position.x+offset_x) ) * Time.deltaTime, 0, 0 );
+        }
+        while (Mathf.Abs(target.position.y - (transform.position.y+offset_y) )  > 1)
+        {
+            transform.position += new Vector3(0,Mathf.Sign(target.position.y - (transform.position.y+offset_y) ) * Time.deltaTime, 0 );
         }
     }
 }
