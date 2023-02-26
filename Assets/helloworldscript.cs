60a6628f79a52752d781b4473e7a58e02734ab97
huy2000vu <104468409+huy2000vu@users.noreply.github.com>
2023-02-26 13:56:29 -0800

    New

--- Assets/helloworldscript.cs
+++ Assets/helloworldscript.cs
@@ -1,29 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using System.IO;
-//using random
+
 
 public class helloworldscript : MonoBehaviour
 {
+    public UnityEvent jump;
+    public UnityEvent kick;
+    public UnityEvent duck;
+    
     string jump;
     string kick;
     string duck;
     // Start is called before the first frame update
     void Start()
     {
+        
         var lines = System.IO.File.ReadLines(@"Assets/message.txt");
-        int randomNum = Random.Range(0,50);
-        foreach (var item in lines) 
+        //foreach (var item in lines) {Debug.Log(item);}
+        List <string> listofWords = new List<string>();
+        foreach(var item in lines)
         {
-            Debug.Log(item);
-            //Debug.Log(lines[randomNum]);
+            listofWords.Add(item);
         }
+        jump = (string)listofWords[Random.Range(0, listofWords.Count)];
+        kick = (string)listofWords[Random.Range(0, listofWords.Count)];
+        duck = (string)listofWords[Random.Range(0, listofWords.Count)];
+        Debug.Log("kick: "+kick);
+        Debug.Log("jump: "+jump);
+        Debug.Log("duck: "+duck);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(input == jump)
+        {
+            jump.Invoke();
+        }
+        if else(input == kick)
+        {
+            kick.Invoke();
+        }
+        else  {duck.Invoke()}
+    }
+
+    public void WordReroll()
+    {
+    jump = (string)listofWords[Random.Range(0, listofWords.Count)];
+    kick = (string)listofWords[Random.Range(0, listofWords.Count)];
+    duck = (string)listofWords[Random.Range(0, listofWords.Count)];
     }
 }
