d3061890ffb31200b856a6c44ad122740b83fdf8
WaterKatLaptop\WaterKat <WaterKatLLC@gmail.com>
2023-02-26 07:55:48 -0800

    Fixed While Loop Bug when timescale below 0.01

--- Assets/_Project/Player/FollowPlayer.cs
+++ Assets/_Project/Player/FollowPlayer.cs
@@ -16,6 +16,8 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        if (Time.timeScale < 0.1f)
+            return;
         while (Mathf.Abs(target.position.x - (transform.position.x+offset_x) )  > 2)
         {
             transform.position += new Vector3(Mathf.Sign(target.position.x - (transform.position.x+offset_x) ) * Time.deltaTime, 0, 0 );
